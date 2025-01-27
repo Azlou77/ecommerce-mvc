360765f08f4a96663bd40af98d2c3f484fe0bddb
Nguyen Louis <nguyenlouis172@gmail.com>
2025-01-27 16:52:14 +0100

    add database connection

---
+++ App/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use PDO;
+use PDOStatement;
+
+class Database
+{
+    private $dsn = "mysql:dbname=ecommerce;host=localhost";
+    private $username  = "root";
+    private $password = "";
+    private $connexion;
+    private $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+    public function __construct()
+    {
+        $this->connexion = new PDO($this->dsn, $this->username, $this->password, $this->option);
+        $this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+    public function query($query, $params = []): bool|PDOStatement
+    {
+        if ($params) {
+            $request = $this->connexion->prepare($query);
+            $request->execute($params);
+        } else {
+            $request = $this->connexion->query($query);
+        }
+        return $request;
+    }
+}
