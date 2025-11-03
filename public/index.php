65a283626f997967ce0c970cf6f074954c61d1d3
Nguyen Louis <nguyenlouis172@gmail.com>
2025-11-03 17:23:36 +0100

    update route category

--- public/index.php
+++ public/index.php
@@ -1,62 +1,59 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-use Ecommerce\Controllers\CategoryController;
 use Ecommerce\Controllers\ContactController;
 use Ecommerce\Controllers\HomeController;
 use Ecommerce\Controllers\ProductController;
-use Ecommerce\Controllers\UserController;
+
 
 
 
 require '../vendor/autoload.php';
 $url = $_GET['url'] ?? '';
-$color = $_GET['color'] ?? '';
-$priceRange = $_GET['priceRange'] ?? '';
+
+/* Séparation url en paramètres
+   Example http://ecommerce.local/shoes = 1 paramètre(shoes)
+   Example http://ecommerce.local/shoes/mocassin 2 paramètres (shoes + mocassin)
+*/
+$params = $url ? explode('/', $url) : [];
+
+
 $idProduct = $_GET['idProduct'] ?? null;
 $categoryName = $_GET['categoryName'] ?? '';
 
-
-
-if ($url && $url == 'productFilteredByColor' && !empty($color)) {
-	$product = new ProductController();
-	$product->productFilteredByColor($color);
-
-} elseif ($url && $url == 'productFilteredByPriceRange' && !empty($priceRange)) {
-	$product = new ProductController();
-	$product->productFilteredByPriceRange();
-
-} elseif ($url && $url == 'contact') {
-	$contact = new ContactController();
-	$contact->index();
-
-} elseif ($url && $url == 'product') {
-	$product = new ProductController();
-	$product->index();
-
-} elseif ($url && $url == 'cart') {
-	$product = new ProductController();
-	$product->getProductsFromCart();
-
-} elseif ($url == 'productDetail' && !empty($idProduct)) {
-	$product = new ProductController();
-	$product->getProduct($idProduct);
+http://ecommerce.local/
+if (empty ($params)) {
+	(new HomeController)->index();
 	
-} elseif ($url && $url == 'category') {
-	$category = new CategoryController();
-	$category->index();
 }
 
-elseif ( $url && $url == 'register') {
-	$user = new UserController();
-	$user->registerUser($email, $username, $password);
+if ($url && $params[0] != '') {
+	switch ($_GET['url']) {
+		case 'contact':
+			(new ContactController)->index();
+			break;
+		case 'product':
+			(new ProductController)->index();
+			break;
+		case 'productDetail':
+			(new ProductController)->getProduct($idProduct);
+			break;
+		
+		
 
-}  elseif ($url) {
-    $product = new ProductController();
-    $product->productsByCategory($url);
-
-	
-} else {
-	$home = new HomeController();
-	$home->index();
+		default:
+			http_response_code(404);
+			echo "Page not found";
+	}
 }
+
+// Produits d'une catégorie: ecommerce.local/shoes
+if (count($params) === 1) {
+    $categorySlug = $params[0];
+    
+    (new ProductController)->productsByCategory($categorySlug);
+    exit;
+}
+
+
+
+
+
